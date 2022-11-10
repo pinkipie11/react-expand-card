5adaf81b74915ed18d752f570b8ec97de8ad128d
pinkipie11 <stani.simic22@gmail.com>
2022-11-10 06:16:21 +0100

    Added readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        transiton={{ layout: { duration: 1, type: 'spring' } }}
+        layout
+        onClick={() => setIsOpen(!isOpen)}
+        className="card"
+        style={{
+          borderRadius: '1rem',
+          boxShadow: '0px 10px 30px rgba(0,0,0,0.5)',
+        }}
+      >
+        <motion.h2 layout="position">Framer Motion 🚀</motion.h2>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1 }}
+              exit={{ opacity: 0 }}
+              className="expand"
+            >
+              <p>
+                lorem20 Ovo je neki tekst. Ne radi mi extenzija pa ću nešto
+                napisati. I još malo.
+              </p>
+              <p>lorem 10 I ovdje ću nešto napisati, samo malo kraće.</p>
+              <button>Hello</button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
